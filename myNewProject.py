d4381749955c1a1987e4c4038456619817700538
Parth Gautam <137072639+Parth150804@users.noreply.github.com>
2024-06-29 17:31:45 +0530

    Bugs fixed in myNewProject.py

--- myNewProject.py
+++ myNewProject.py
@@ -15,7 +15,7 @@ while True:
     if (len(arucoFound[0]) != 0):
         for bbox, id in zip(arucoFound[0], arucoFound[1]):
             if int(id) in augDics.keys():
-                img = augmentAruco(bbox, id, img, augDics[int(id)])
+                img = arm.augmentAruco(bbox, id, img, augDics[int(id)])
 
     cv2.imshow("Image", img)
     cv2.waitKey(1)
